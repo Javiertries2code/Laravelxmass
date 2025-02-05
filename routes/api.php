f4d325126500610ca5105890f55d738f02fe0fcc
Javiertries2code <superjabo@live.com>
2025-02-05 23:54:46 +0100

    opsie

--- routes/api.php
+++ routes/api.php
@@ -17,35 +17,35 @@ use App\Http\Controllers\API\SubjectController;
 
 
 
-Route::get('/api/users', [UserController::class, 'index'])->middleware(['can:admin', 'can:god']);
-Route::get('/api/users/{user}', [UserController::class, 'show'])->middleware(['can:admin', 'can:god']);
-Route::post('/api/users', [UserController::class, 'store'])->middleware(['can:admin', 'can:god']);
-Route::put('/api/users/{user}', [UserController::class, 'update'])->middleware(['can:admin', 'can:god']);
-Route::delete('/api/users/{user}', [UserController::class, 'destroy'])->middleware(['can:admin', 'can:god']);
-
-Route::get('/api/courses', [CourseController::class, 'index'])->middleware(['can:todos']);
-Route::get('/api/courses/{course}', [CourseController::class, 'show'])->middleware(['can:todos']);
-Route::post('/api/courses', [CourseController::class, 'store'])->middleware(['can:admin', 'can:god']);
-Route::put('/api/courses/{course}', [CourseController::class, 'update'])->middleware(['can:admin', 'can:god']);
-Route::delete('/api/courses/{course}', [CourseController::class, 'destroy'])->middleware(['can:admin', 'can:god']);
-
-Route::get('/api/subjects', [SubjectController::class, 'index'])->middleware(['can:todos']);
-Route::get('/api/subjects/{subject}', [SubjectController::class, 'show'])->middleware(['can:todos']);
-Route::post('/api/subjects', [SubjectController::class, 'store'])->middleware(['can:admin', 'can:god']);
-Route::put('/api/subjects/{subject}', [SubjectController::class, 'update'])->middleware(['can:admin', 'can:god']);
-Route::delete('/api/subjects/{subject}', [SubjectController::class, 'destroy'])->middleware(['can:admin', 'can:god']);
 
 
-Route::get('/api/schedules', [ScheduleController::class, 'index'])->middleware(['can:admin', 'can:god', 'can:teacher']);
-Route::get('/api/schedules/{schedule}', [ScheduleController::class, 'show'])->middleware(['can:admin', 'can:god', 'can:teacher']);
-Route::post('/api/schedules', [ScheduleController::class, 'store'])->middleware(['can:admin', 'can:god']);
-Route::put('/api/schedules/{schedule}', [ScheduleController::class, 'update'])->middleware(['can:admin', 'can:god']);
-Route::delete('/api/schedules/{schedule}', [ScheduleController::class, 'destroy'])->middleware(['can:admin', 'can:god']);
+Route::get('/users', [UserController::class, 'index'])->middleware(['can:admin', 'can:god']);
+Route::get('/users/{user}', [UserController::class, 'show'])->middleware(['can:admin', 'can:god']);
+Route::post('/users', [UserController::class, 'store'])->middleware(['can:admin', 'can:god']);
+Route::put('/users/{user}', [UserController::class, 'update'])->middleware(['can:admin', 'can:god']);
+Route::delete('/users/{user}', [UserController::class, 'destroy'])->middleware(['can:admin', 'can:god']);
 
-Route::get('/api/studentschedules', [StudentScheduleController::class, 'index'])->middleware(['can:student']);
-Route::get('/api/studentschedules/{studentschedule}', [StudentScheduleController::class, 'show'])->middleware(['can:student']);
+Route::get('/courses', [CourseController::class, 'index'])->middleware(['can:todos']);
+Route::get('/courses/{course}', [CourseController::class, 'show'])->middleware(['can:todos']);
+Route::post('/courses', [CourseController::class, 'store'])->middleware(['can:admin', 'can:god']);
+Route::put('/courses/{course}', [CourseController::class, 'update'])->middleware(['can:admin', 'can:god']);
+Route::delete('/courses/{course}', [CourseController::class, 'destroy'])->middleware(['can:admin', 'can:god']);
 
-Route::post('/api/studentschedules', [StudentScheduleController::class, 'store'])->middleware(['can:admin', 'can:god']);
-Route::put('/api/studentschedules/{studentschedule}', [StudentScheduleController::class, 'update'])->middleware(['can:admin', 'can:god']);
-Route::delete('/api/studentschedules/{studentschedule}', [StudentScheduleController::class, 'destroy'])->middleware(['can:admin', 'can:god']);
+Route::get('/subjects', [SubjectController::class, 'index'])->middleware(['can:todos']);
+Route::get('/subjects/{subject}', [SubjectController::class, 'show'])->middleware(['can:todos']);
+Route::post('/subjects', [SubjectController::class, 'store'])->middleware(['can:admin', 'can:god']);
+Route::put('/subjects/{subject}', [SubjectController::class, 'update'])->middleware(['can:admin', 'can:god']);
+Route::delete('/subjects/{subject}', [SubjectController::class, 'destroy'])->middleware(['can:admin', 'can:god']);
+
+Route::get('/schedules', [ScheduleController::class, 'index'])->middleware(['can:admin', 'can:god', 'can:teacher']);
+Route::get('/schedules/{schedule}', [ScheduleController::class, 'show'])->middleware(['can:admin', 'can:god', 'can:teacher']);
+Route::post('/schedules', [ScheduleController::class, 'store'])->middleware(['can:admin', 'can:god']);
+Route::put('/schedules/{schedule}', [ScheduleController::class, 'update'])->middleware(['can:admin', 'can:god']);
+Route::delete('/schedules/{schedule}', [ScheduleController::class, 'destroy'])->middleware(['can:admin', 'can:god']);
+
+Route::get('/studentschedules', [StudentScheduleController::class, 'index'])->middleware(['can:student']);
+Route::get('/studentschedules/{studentschedule}', [StudentScheduleController::class, 'show'])->middleware(['can:student']);
+Route::post('/studentschedules', [StudentScheduleController::class, 'store'])->middleware(['can:admin', 'can:god']);
+Route::put('/studentschedules/{studentschedule}', [StudentScheduleController::class, 'update'])->middleware(['can:admin', 'can:god']);
+Route::delete('/studentschedules/{studentschedule}', [StudentScheduleController::class, 'destroy'])->middleware(['can:admin', 'can:god']);
 
